0fcdadf3350ac1129450b4812f2f745543254db7
Sandip <c.restsandip123@gmail.com>
2023-12-31 17:22:37 +0545

    fixing issue related title meta tag etc.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,16 +3,23 @@ import Layout from '@/components/Layout';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { store, persistor } from '@/redux/Store';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  // persistor.purge();
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </PersistGate>
-    </Provider>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/images/favicon_io/favicon-16x16.png" type="image/png" size="16x16" />
+        <link rel="shortcut icon" href="/images/favicon_io/favicon-32x32.png" type="image/png" size="32x32" />
+        <title>Maxima international Pvt. Ltd</title>
+      </Head>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </PersistGate>
+      </Provider>
+    </>
   )
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,11 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="shortcut icon" href="/images/favicon_io/favicon-16x16.png" type="image/png" size="16x16" />
-        <link rel="shortcut icon" href="/images/favicon_io/favicon-32x32.png" type="image/png" size="32x32" />
-        {/* <title>Maxima Multinational Trading Pvt. Ltd.</title> */}
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
